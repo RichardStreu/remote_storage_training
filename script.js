193a6472b012e37e987b3cfe8fa9f50e1d0f7b78
Richard Streu <rstreu13@icloud.com>
2024-09-25 11:22:01 +0200

    PUT ready

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function init() {
   console.log("Hallo Welt");
   // loadData(pathFruit1Type);
-  loadData();
+  // loadData();
 }
 
 let pathFruit1Type = "/fruits/fruit1/type";
@@ -12,12 +12,11 @@ const BASE_URL = "https://remotestorage-c3cfa-default-rtdb.europe-west1.firebase
 // ###############       fetch GET      ###############################################
 // #####################################################################################
 
-// man kann hier einen path übergeben, der by default ein leerer string ist, sodass die api auch abgerufen wird, wenn kein direkter path als parameter beim funktionsaufruf übergeben wird.
+// man kann hier einen path übergeben, der by default ein leerer string ist, sodass die api auch    abgerufen wird, wenn kein direkter path als parameter beim funktionsaufruf übergeben wird.
 // man kann dann aber beim funktionsaufruf einen pfad übergeben, welcher quasi in die gesamte url eingebunden wird und uns dann das gewünschte lement ausgibt. in zukunft die einzelnen zwischenpfade vielleicht sogar als variable speichern.
 
 async function loadData(path = "") {
   let response = await fetch(BASE_URL + path + ".json");
-  console.log(response);
   if (!response.ok) return;
   return (responseToJson = await response.json());
 }
@@ -57,7 +56,7 @@ let newFruitPath = "/fruits";
 // #####################################################################################
 
 // VORSICHT MIT default leerem Path!!!  Wenn diese Funktion so versehentlich abfeuert, wird das komplette Objekt in der Datenbank (alle Daten) gelöscht!!!
-async function deleteData(path = "") {
+async function deleteData(path) {
   let response = await fetch(BASE_URL + path + ".json", {
     method: "DELETE",
   });
@@ -66,6 +65,30 @@ async function deleteData(path = "") {
 
 // deleteData("/fruits/-O7c-kRZHZVZfBdAopov");
 
-// #####################################################################################
-// ###############       fetch POST      ###############################################
-// #####################################################################################
+// ####################################################################################
+// ###############       fetch PUT      ###############################################
+// ####################################################################################
+
+async function putData(path = "", data) {
+  const response = await fetch(BASE_URL + path + ".json", {
+    method: "PUT",
+    header: {
+      "Content-Type": "Application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  if (!response.ok) return;
+}
+
+let putPath = "/fruits";
+let putDataObject = {
+  fruit4: {
+    type: "Apple",
+    name: "Pink Lady",
+    count: 150,
+  },
+};
+
+putData(putPath, putDataObject);
+
+// ACHTUNG: Mit PUT kann man wirklich nur gewisse elemente ersetzen! Sollten mehrere Objekte auf einer Ebene liegen und an möchte mit PUT ein weitere Objekt hinzufügen, werden die anderen Objekte gelöscht und durch das neue ersetzt!
